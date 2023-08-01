28b17fcbcc1d5828b576a3e77fb841c1461b58a7
akapi83 <akapi83@gmail.com>
2023-08-01 22:28:06 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const swpier = new Swiper('.swpier', {
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 4000,
+  },
+  centeredSlides: true,
+  slidesPerView: 1.5,
+  spaceBetween: 20,
+
+});
